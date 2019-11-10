672f689108bbb6e76db4bee78d6abdd2dc99381a
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2019-11-10 22:56:01 +0100

    Switched to Click CLI

--- Main.py
+++ Main.py
@@ -1,101 +1,105 @@
-from sys import argv
-from os import getcwd
+from click import pass_context, option, group
 from src.ciphers.CaesarZ26 import CaesarZ26
 from src.ciphers.AffineZ26 import AffineZ26
 from src.ciphers.VigenereZ26 import VigenereZ26
+from src.utils.utils import read_file, write_file
 
-CIPHERS = {
-    "-c": CaesarZ26,
-    "-a": AffineZ26,
-    "-v": VigenereZ26
-}
+# Functionality Support, used in --help
+ENCRYPTION = ['Caesars', 'Affine', 'Vigenere']
+DECRYPTION = ['Caesars', 'Affine', 'Vigenere']
+CRYPTOANALYSIS = ['Caesars', 'Affine', 'Vigenere']
+BRUTEFORCE = ['Caesars', 'Affine']
 
-FLAGS = {"-e", "-d", "-j", "-k"}
-
-FILEPATHS = {
-    "plain": "\\text_files\\plain.txt",
-    "decrypt": "\\text_files\\decrypt.txt",
-    "crypto": "\\text_files\\crypto.txt",
-    "key": "\\text_files\\key.txt",
-    "new-key": "\\text_files\\new-key.txt",
-    "extra": "\\text_files\\extra.txt",
-}
+# Filepaths
+ORIGINALTEXT = ['src', 'text_files', 'orig.txt']
+PLAINTEXT = ['src', 'text_files', 'plain.txt']
+EXTRATEXT = ['src', 'text_files', 'extra.txt']
+KEY = ['src', 'text_files', 'key.txt']
+NEWKEY = ['src', 'text_files', 'new-key.txt']
+ENCRYPTED = ['src', 'text_files', 'crypto.txt']
+DECRYPTED = ['src', 'text_files', 'decrypt.txt']
 
 
-def main(argv):
+@group(chain=True)
+@option('-c', 'cipher', flag_value=CaesarZ26, help='Ceasars Cipher')
+@option('-a', 'cipher', flag_value=AffineZ26, help='Affine Cipher')
+@option('-v', 'cipher', flag_value=VigenereZ26, help='Vigenere Cipher')
+@pass_context
+def main(ctx, cipher):
+    '''
+    \b
+    Cryptography Script
+    \b
+    Expects two arguments,
+    first one for the cipher you wish to use
+    and the second one for desired functionality.
+    \b
+    Example Use Case:
+    You want to encrypt text using Vigenere Cipher.
 
-    if len(argv) != 2:
-        raise ValueError("You need to pass exactly two arguments.")
+    >>> python main.py -v e
+
+    Notice how the '-' sign is missing on the second arg.
+    '''
+    ctx.ensure_object(dict)
+    ctx.obj['CIPHER'] = cipher
+
+
+@main.command(
+    'e',
+    help=f'Encrypting, Supported: {ENCRYPTION}'
+)
+@pass_context
+def encrypt(ctx):
+    cipher = ctx.obj['CIPHER']
+    text = read_file(PLAINTEXT)
+    key = read_file(KEY)
+    encrypted_text = cipher.encrypt(text, key)
+    write_file(encrypted_text, ENCRYPTED)
+
+
+@main.command(
+    'd',
+    help=f'Decrypting, Supported: {DECRYPTION}'
+)
+@pass_context
+def decrypt(ctx):
+    cipher = ctx.obj['CIPHER']
+    text = read_file(ENCRYPTED)
+    key = read_file(KEY)
+    encrypted_text = cipher.encrypt(text, key)
+    write_file(encrypted_text, DECRYPTED)
+
+
+@main.command(
+    'k',
+    help=f'Cryptoanalysis, Supported: {CRYPTOANALYSIS}'
+)
+@pass_context
+def cryptoanalysis(ctx):
+    cipher = ctx.obj['CIPHER']
+    text = read_file(ENCRYPTED)
+    if isinstance(cipher, (AffineZ26, CaesarZ26)):
+        plain_sample = read_file(EXTRATEXT)
+        decrypted_text, key = cipher.cryptoanalysis(text, plain_sample)
     else:
-        cipher, flag = argv
-
-        if cipher in CIPHERS.keys() and flag in FLAGS:
-            cipher = CIPHERS[cipher]
-        else:
-            raise ValueError(
-                f'Combination of this cipher {cipher}' +
-                f'and this flag {flag} is not supported.'
-            )
-
-        if flag == "-e":
-            text = read_file(FILEPATHS["plain"])
-            keys = get_keys(read_file(FILEPATHS["key"]))
-            encrypted = cipher.encrypt(text, keys)
-            write_file(encrypted, FILEPATHS["crypto"])
-
-        elif flag == "-d":
-            text = read_file(FILEPATHS["crypto"])
-            keys = get_keys(read_file(FILEPATHS["key"]))
-            decrypted = cipher.decrypt(text, keys)
-            write_file(decrypted, FILEPATHS["decrypt"])
-
-        elif flag == "-j":
-            encrypted = read_file(FILEPATHS["crypto"])
-            extra = read_file(FILEPATHS["extra"])
-            key, decrypted = cipher.cryptoanalysis(encrypted, extra)
-            write_file(decrypted, FILEPATHS["decrypt"])
-            write_file(str(key), FILEPATHS["new-key"])
-
-        elif flag == "-k":
-            encrypted = read_file(FILEPATHS["crypto"])
-            decryptions = cipher.bruteforce(encrypted)
-            write_file(decryptions, FILEPATHS["decrypt"])
+        key = cipher.cryptoanalysis(text)
+        decrypted_text = cipher.decrypt(text, key)
+    write_file(decrypted_text, DECRYPTED)
+    write_file(key, NEWKEY)
 
 
-def read_file(filename):
-    with open(getcwd() + filename, "r") as ifile:
-        text = ifile.read()
-        if text == "":
-            raise ValueError(f"File {filename} is empty")
-        else:
-            return text
+@main.command(
+    'j',
+    help=f'Bruteforce Deciphering, Supported: {BRUTEFORCE}'
+)
+@pass_context
+def bruteforce(ctx):
+    cipher = ctx.obj['CIPHER']
+    encrypted = read_file(ENCRYPTED)
+    decryptions = cipher.bruteforce(encrypted)
+    write_file(decryptions, DECRYPTED)
 
 
-def write_file(text, filename):
-    with open(getcwd() + filename, "w") as ofile:
-        open(getcwd() + filename, "w").close()  # clear output file
-        ofile.write(text)
-
-
-def get_keys(text):
-    try:
-        keys = [int(key) for key in text.split(" ")]
-        return tuple(keys)
-    except ValueError:
-        raise ValueError("Provided keys are invalid")
-
-
-def normalize(text):
-
-    return text
-
-
-if __name__ == "__main__":
-    from sys import version_info as vrs
-
-    if vrs < (3, 8):
-        raise SystemError(
-            "You need at least Python 3.8.0 to run this script. "
-            f"Your current version {vrs.major}.{vrs.minor}.{vrs.micro}"
-        )
-    main(argv[1:])
+if __name__ == '__main__':
+    main()
