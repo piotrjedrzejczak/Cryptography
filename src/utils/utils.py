03650a1ab5f829803a175b1528b2c4e6e7136634
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2019-11-30 15:29:13 +0100

    refactoring names & xor implementation

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,22 +2,43 @@ from os import path, getcwd
 from re import sub
 
 
-def read_file(filepath):
-    with open(path.join(getcwd(), *filepath), "r", encoding='utf-8') as f:
-        text = f.read()
-        if text:
-            return text
-        else:
-            raise ValueError(f"File {filepath[-1]} is empty.")
+def readf(filepath, binary=False):
+    if binary:
+        with open(path.join(getcwd(), *filepath), 'rb') as bf:
+            text = bf.read()
+    else:
+        with open(path.join(getcwd(), *filepath), 'r', encoding='utf-8') as f:
+            text = f.read()
+    if text:
+        return text
+    else:
+        raise ValueError(f"File {filepath[-1]} is empty.")
 
 
-def write_file(text, filepath):
-    with open(path.join(getcwd(), *filepath), "w") as f:
-        # Clearing file
-        open(path.join(getcwd(), *filepath), "w").close()
-        f.write(text)
+def writef(text, filepath, binary=False):
+    if binary:
+        f = open(path.join(getcwd(), *filepath), 'wb')
+    else:
+        f = open(path.join(getcwd(), *filepath), 'w')
+    # Clearing file
+    open(path.join(getcwd(), *filepath), 'w').close()
+    f.write(text)
 
 
 def normalize_text(text):
     '''Returns lowercase, stripped text with spaces.'''
     return sub(r'[^a-zA-Z ]+', '', text).strip().lower()
+
+
+def xor(vec1, vec2):
+    ''' XOR's two vectors, if they're of different length,
+        shorter one will be padded with zeros.
+        Returns bytearray.
+    '''
+    if len(vec1) != len(vec2):
+        diff = abs(len(vec1)-len(vec2))
+        if len(vec1) < len(vec2):
+            vec1 += bytearray(diff)
+        else:
+            vec2 += bytearray(diff)
+    return bytearray([vec1[i] ^ vec2[i] for i in range(len(vec1))])
